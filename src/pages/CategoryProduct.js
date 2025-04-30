64134d77af3ca3f98c8c6d1a139d230d3f2890a9
Mureithi-Code <darkknight505c@example.com>
2025-04-30 13:40:31 +0300

    Incomplete

--- src/pages/CategoryProduct.js
+++ src/pages/CategoryProduct.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import productCategory from '../helpers/productCategory'
 import VerticalCard from '../components/VerticalCard'
 import SummaryApi from '../common'
@@ -7,7 +7,7 @@ import SummaryApi from '../common'
 const CategoryProduct = () => {
     const [data,setData] = useState([])
     const navigate = useNavigate()
-    const [loading,setLoading] = useState(false)
+    const [loading] = useState(false)
     const location = useLocation()
     const urlSearch = new URLSearchParams(location.search)
     const urlCategoryListinArray = urlSearch.getAll("category")
