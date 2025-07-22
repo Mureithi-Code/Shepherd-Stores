73311876efae0cec46bf422a67dc3717d56439a3
Mureithi-Code <darkknight505c@example.com>
2025-07-22 18:54:09 +0300

    updated homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,17 +9,20 @@ const Home = () => {
       <CategoryList/>
       <BannerProduct/>
 
-      <HorizontalCardProduct category={"airpodes"} heading={"Top's Airpodes"}/>
-      <HorizontalCardProduct category={"watches"} heading={"Popular's Watches"}/>
+      <HorizontalCardProduct category="airpodes" heading="Top's Airpodes" />
+      <HorizontalCardProduct category="watches" heading="Popular Watches" />
 
-      <VerticalCardProduct category={"mobiles"} heading={"Mobiles"}/>
-      <VerticalCardProduct category={"Mouse"} heading={"Mouse"}/>
-      <VerticalCardProduct category={"televisions"} heading={"Televisions"}/>
-      <VerticalCardProduct category={"camera"} heading={"Camera & Photography"}/>
-      <VerticalCardProduct category={"earphones"} heading={"Wired Earphones"}/>
-      <VerticalCardProduct category={"speakers"} heading={"Bluetooth Speakers"}/>
-      <VerticalCardProduct category={"refrigerator"} heading={"Refrigerator"}/>
-      <VerticalCardProduct category={"trimmers"} heading={"Trimmers"}/>
+      <VerticalCardProduct category="mobiles" heading="Mobiles" />
+      <VerticalCardProduct category="mouse" heading="Mouse" />
+      <VerticalCardProduct category="televisions" heading="Televisions" />
+      <VerticalCardProduct category="camera" heading="Camera & Photography" />
+      <VerticalCardProduct category="earphones" heading="Wired Earphones" />
+      <VerticalCardProduct category="speakers" heading="Bluetooth Speakers" />
+      <VerticalCardProduct category="refrigerator" heading="Refrigerator" />
+      <VerticalCardProduct category="trimmers" heading="Trimmers" />
+      <VerticalCardProduct category="health" heading="Health" />
+      <VerticalCardProduct category="beauty" heading="Beauty Products" />
+      <VerticalCardProduct category="Organic-fertilizer" heading="Organic Fertilizer" />
     </div>
   )
 }
